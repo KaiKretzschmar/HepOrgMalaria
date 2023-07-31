f0e084ab95751a29ea4528230f62d4a66350c8bd
Kai Kretzschmar <34773659+KaiKretzschmar@users.noreply.github.com>
2023-07-31 17:20:31 +0200

    Update 3_HepOrg_parasite_reads.R

--- 3_HepOrg_parasite_reads.R
+++ 3_HepOrg_parasite_reads.R
@@ -13,3 +13,26 @@ library(magrittr)
 
 ##Set working directory
 setwd("~/HepOrgMalaria/parasitereads/")
+
+
+##Load in rawdata and metadata
+rawdata <- read.csv("HepOrgMalaria_parasitereads_rawdata.csv",sep=",")
+names   <- make.unique(rawdata[,1])
+rownames(rawdata) <- names
+rawdata <- rawdata[,-1]
+
+rawdata[is.na(rawdata)] <- 0
+
+rawdata <- rawdata[grep("ERCC-",rownames(rawdata), invert=TRUE),]
+rawdata <- rawdata[grep("MT-",rownames(rawdata), invert=TRUE),]
+
+
+metadata <- read.csv("HepOrgMalaria_parasitereads_metadata.csv", sep=",")
+rownames(metadata) <- metadata[,1]
+metadata <- metadata[,-1]
+
+
+#Creat Seurat object
+pbmc <- CreateSeuratObject(counts = rawdata, meta.data = metadata)
+pbmc <- subset(x = pbmc, subset = nCount_RNA > 50 & nCount_RNA < Inf)
+
