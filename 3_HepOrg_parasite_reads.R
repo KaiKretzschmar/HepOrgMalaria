db7ee4448d78d7299969071e5ac975f47dd19be3
Kai Kretzschmar <34773659+KaiKretzschmar@users.noreply.github.com>
2023-07-31 17:19:10 +0200

    Update 3_HepOrg_parasite_reads.R

--- 3_HepOrg_parasite_reads.R
+++ 3_HepOrg_parasite_reads.R
@@ -1,1 +1,15 @@
-setwd("~/Dropbox/_Malaria RNA-seq/Plasmodium single-cell analysis/")
+##Load packages
+library(Seurat)
+library(ggplot2)
+library(sctransform)
+library(DESeq2)
+library(RColorBrewer)
+library(enrichR)
+library(clustree)
+library(DoMultiBarHeatmap)
+library(rlang)
+library(magrittr)
+
+
+##Set working directory
+setwd("~/HepOrgMalaria/parasitereads/")
