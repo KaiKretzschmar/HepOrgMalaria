935d81c29e424be692f0ebe3dc2db7326b63c3d5
Kai Kretzschmar <34773659+KaiKretzschmar@users.noreply.github.com>
2023-07-31 18:04:30 +0200

    Update 3_HepOrg_parasite_reads.R

--- 3_HepOrg_parasite_reads.R
+++ 3_HepOrg_parasite_reads.R
@@ -105,5 +105,13 @@ DoMultiBarHeatmap(pbmc,
                  ) 
 + scale_fill_gradientn(colors = viridis(10))
 
+#Plot selected DE genes up in liver stage
+pdf("Figure_4D_liver_stage_genes.pdf")
+my_comparisonp <- list( c("liver", "blood"))
+csp <- VlnPlot(pbmc, log = T, features = "XM001351086.1", cols = c("#5ab4ac","#d8b365"), group.by = "stage", pt.size = 0) + geom_boxplot(width=0.2,fill="white") + stat_compare_means(method = "wilcox.test", label = "p.signif", comparisons = my_comparisonp) + theme(legend.position = "none") + ggtitle('CSP (PF3D7_0304600)') 
+lisp <- VlnPlot(pbmc, log = T, features = "XM001348316.1", cols = c("#5ab4ac","#d8b365"), group.by = "stage", pt.size = 0) + geom_boxplot(width=0.2,fill="white") + stat_compare_means(method = "wilcox.test", label = "p.signif", comparisons = my_comparisonp) + theme(legend.position = "none") + ggtitle('LISP1 (PF3D7_1418100)') 
+slarp <- VlnPlot(pbmc, log = T, features = "XM001348111.1", cols = c("#5ab4ac","#d8b365"), group.by = "stage", pt.size = 0) + geom_boxplot(width=0.2,fill="white") + stat_compare_means(method = "wilcox.test", label = "p.signif", comparisons = my_comparisonp) + theme(legend.position = "none") + ggtitle('SLARP (PF3D7_1147000)') 
+CombinePlots(list(csp,lisp,slarp),ncol = 3)
+dev.off()
 
 
