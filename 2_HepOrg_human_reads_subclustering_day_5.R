007e6f1267ee39c0cd49774aa1093ea99b00b145
Kai Kretzschmar <34773659+KaiKretzschmar@users.noreply.github.com>
2023-07-31 14:48:12 +0200

    Update 2_HepOrg_human_reads_subclustering_day_5.R

--- 2_HepOrg_human_reads_subclustering_day_5.R
+++ 2_HepOrg_human_reads_subclustering_day_5.R
@@ -1,4 +1,14 @@
+##Load packages
+library(Seurat)
+library(ggplot2)
+library(sctransform)
+library(DESeq2)
+library(RColorBrewer)
+library(enrichR)
+library(clustree)
 library(DoMultiBarHeatmap)
+library(rlang)
+library(magrittr)
 
 
 ##Set working directory
